68e05c4e7240ceed3b4207e9d19b102fe71540af
Rikkie <rikkie@localhost.localdomain>
2018-06-02 01:56:59 -0400

    v1.1.1

--- test.py
+++ test.py
@@ -3,18 +3,14 @@ import speech_recognition as sr
 # create a recognizer
 r = sr.Recognizer()
 mic = sr.Microphone()
-
+sr.Microphone.list_microphone_names()
 L=''
 while L=='':
-    print('say something')
+    print('Hello tell me how you\'re feeling today')
     with mic as source:
         audio = r.listen(source)
     try:
         L=r.recognize_google(audio)
     except sr.UnknownValueError:
-        print('Sorry we didnt capture your words')
-print(L)
-if L == 'hello':
-    print('hey pal')
-elif L!='': print('nothing')
-elif L=='':print('empty')
+        print('Sorry I could not hear you. Could you please repeat?')
+print("I will try and help you with".L)
