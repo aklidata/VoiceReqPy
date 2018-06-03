91fd5091aad2fe65e5e18aad552be8ae8082475b
zack <aklidata.co@gmail.com>
2018-06-02 23:42:47 -0400

    Mainold

--- test-french.py
+++ test-french.py
@@ -4,17 +4,25 @@ import speech_recognition as sr
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-L=''
-while L=='':
-    print('Bonjour!')
-    with mic as source:
-        audio = r.listen(source)
-    try:
-        L=r.recognize_google(audio,language='fr-FR')
-    except sr.UnknownValueError:
-        print('Desole, je n\'ai rien compris')
-print(L)
+
+print('Bonjour')
+def inquire(x):
+    L= x
+    while L=='':
+
+        with mic as source:
+            audio = r.listen(source)
+        try:
+            L=r.recognize_google(audio,language='fr-FR')
+        except sr.UnknownValueError:
+            print('Desole, je n\'ai rien compris')
+    if L!='Bonjour':print(L)
+    return L
+L = inquire('')
+
+
 if L == 'Bonjour':
     print('Est ce que je peux vous aider?')
-elif L!='': print('Vous N\'avais rien dit')
-elif L=='':print('Vide')
+    inquire('')
+#elif L!='': print('Vous N\'avais rien dit')
+#elif L=='':print('Vide')
